6284b6757035d4fc80c9a3358b06c0e5573aa28a
Aleksey Kovtunenko <alexkovtunenkogm@gmail.com>
2025-07-13 20:27:51 +0300

    add logger package

---
+++ internal/logger/logger.go
@@ -0,0 +1,73 @@
+package logger
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func NewLogger() (*zap.Logger, error) {
+	var cfg zap.Config
+
+	cfg = zap.NewDevelopmentConfig()
+
+	l, err := cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+
+func (w *loggingResponseWriter) WriteHeader(statusCode int) {
+	w.responseData.status = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := w.ResponseWriter.Write(b)
+	w.responseData.size += size
+	return size, err
+}
+
+func LoggingMiddleware(l *zap.Logger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		logFn := func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			responseData := &responseData{
+				status: 0,
+				size:   0,
+			}
+			lw := loggingResponseWriter{
+				ResponseWriter: w,
+				responseData:   responseData,
+			}
+			h.ServeHTTP(&lw, r)
+
+			duration := time.Since(start)
+
+			l.Info("",
+				zap.String("uri", r.RequestURI),
+				zap.String("method", r.Method),
+				zap.Int("status", responseData.status),
+				zap.Duration("duration", duration),
+				zap.Int("size", responseData.size),
+			)
+		}
+		return http.HandlerFunc(logFn)
+	}
+}
